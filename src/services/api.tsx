40c8aa7330207c2cbea18ec3a592c54a86bdf38d
lowqualityplays <lqplays@outlook.com>
2022-08-20 19:27:18 -0300

    :recycle: refactor: Changed behavior of getBets function.

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,9 +1,12 @@
-function getBets(): any {
-  return fetch('http://www.mocky.io/v2/59f08692310000b4130e9f71')
+function getBets(): EventType[] {
+  let result: EventType[] = [];
+  fetch('http://www.mocky.io/v2/59f08692310000b4130e9f71')
     .then(res => res.json())
     .then(res => {
-      return res as EventType[];
+      result = res as EventType[];
     });
+
+  return result;
 }
 
 export default getBets;
