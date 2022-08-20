410f0f26c5d05aada3843dff722a66c4abaae59c
lowqualityplays <lqplays@outlook.com>
2022-08-20 19:49:49 -0300

    :recycle: refactor: fixed some problems in getBets

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,12 +1,7 @@
-function getBets(): EventType[] {
-  let result: EventType[] = [];
-  fetch('http://www.mocky.io/v2/59f08692310000b4130e9f71')
+async function getBets(): Promise<EventType[]> {
+  return await fetch('http://www.mocky.io/v2/59f08692310000b4130e9f71')
     .then(res => res.json())
-    .then(res => {
-      result = res as EventType[];
-    });
-
-  return result;
+    .then((res: any) => res as EventType[]);
 }
 
 export default getBets;
