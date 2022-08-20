33b9c28cde786238eda5f04e4c7f39c34c3fb7ff
lowqualityplays <lqplays@outlook.com>
2022-08-20 20:03:12 -0300

    :recycle: refactor: Removed unnecessary code from getBets

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 async function getBets(): Promise<EventType[]> {
-  return await fetch('http://www.mocky.io/v2/59f08692310000b4130e9f71')
-    .then(res => res.json())
-    .then((res: any) => res as EventType[]);
+  return await fetch('http://www.mocky.io/v2/59f08692310000b4130e9f71').then(
+    res => res.json()
+  );
 }
 
 export default getBets;
