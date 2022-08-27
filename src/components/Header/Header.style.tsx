de7a1c7a04b410276609b1aa60ea274fd0a23f26
lowqualityplays <lqplays@outlook.com>
2022-08-27 14:46:52 -0300

    feat(header component): adds header component with button for betslip

---
+++ src/components/Header/Header.style.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Container = styled('div')`
+  position: sticky;
+  display: flex;
+  top: 0;
+  outline: 1px solid black;
+  padding: 5px 10px;
+  background: white;
+  justify-content: flex-end;
+`;
+
+export const Button = styled('button')`
+  padding: 5px 10px;
+`;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button, Container } from './Header.style';
+
+function Header() {
+  return (
+    <Container>
+      <Button>=</Button>
+    </Container>
+  );
+}
+
+export default Header;
