30973f1eb9e13bd6247f61435bd0737f2ae32953
lowqualityplays <lqplays@outlook.com>
2022-08-27 11:37:32 -0300

    fix(market test): fixes market component import inside test

--- src/components/market/market.test.tsx
+++ src/components/market/market.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, cleanup } from '@testing-library/react';
-import Market from '../market';
+import Market from './market';
 
 const MockSelection1: SelectionType = {
   id: 'SEL_1',
