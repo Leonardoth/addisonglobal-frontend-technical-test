f78e6c60ec1d82d88027e6e1ef4ab9fbddcbca13
lowqualityplays <lqplays@outlook.com>
2022-08-29 16:43:15 -0300

    feat(actioncreators): adds a new interface for selectionType
    Action payloads will have marketName and Id to keep track of market selection
    
    BREAKING CHANGE: Can affect any component that calls actions

--- src/store/actionCreators.ts
+++ src/store/actionCreators.ts
@@ -2,7 +2,12 @@ import * as actionTypes from './actionTypes';
 import { SelectionType } from '../types/types';
 import { Dispatch } from '@reduxjs/toolkit';
 
-export function addBet(bet: SelectionType) {
+export interface Bet extends SelectionType {
+  marketName: string;
+  marketId: string;
+}
+
+export function addBet(bet: Bet) {
   return (dispatch: Dispatch) => {
     dispatch({
       type: actionTypes.ADD_BET,
@@ -11,7 +16,7 @@ export function addBet(bet: SelectionType) {
   };
 }
 
-export function removeBet(bet: SelectionType) {
+export function removeBet(bet: Bet) {
   return (dispatch: Dispatch) => {
     dispatch({
       type: actionTypes.REMOVE_BET,
