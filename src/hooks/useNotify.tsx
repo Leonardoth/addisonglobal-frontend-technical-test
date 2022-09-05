5ebeca30d62e00833ebcdf44919815ab71af8fac
lowqualityplays <lqplays@outlook.com>
2022-09-05 12:13:02 -0300

    feat(useNotify): Adds NotificationTypes to useNotify hook.

--- src/hooks/useNotify.tsx
+++ src/hooks/useNotify.tsx
@@ -2,6 +2,11 @@ import React, { useContext } from 'react';
 import { UiContext, UiContextProps } from '../context/ContextProvider';
 import { SelectionType } from '../types/types';
 
+export const NotificationTypes = {
+  REMOVE_BET: 'REMOVED_BET',
+  ADD_BET: 'ADDED_BET',
+};
+
 interface useNotifyProps {
   type: string;
   payload: {
@@ -15,13 +20,13 @@ function useNotify() {
 
   function getNotification({ type, payload }: useNotifyProps) {
     switch (type) {
-      case 'REMOVED_BET':
+      case NotificationTypes.REMOVE_BET:
         return {
           id: `REMOVED:${payload.bet.id}`,
           title: 'Removed Bet',
           message: `${payload.marketName}: ${payload.bet.name} (${payload.bet.price})`,
         };
-      case 'ADDED_BET':
+      case NotificationTypes.ADD_BET:
         return {
           id: `ADDED:${payload.bet.id}`,
           title: 'Bet Added',
